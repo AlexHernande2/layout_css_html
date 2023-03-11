ffd21e9e25e46a36c4e0fa32162fc0e5b1b5fe80
Sala D16 <CSalaD16@uniboyaca.edu.co>
2023-03-11 09:54:55 -0500

    Trabajo con js y creación de boton con alerta

---
+++ js/saludar.js
@@ -0,0 +1,13 @@
+// document.getElementById('nombre').setAttribute('value', 'Jhoan');
+
+document.getElementById('saludarBtn').addEventListener('click' , ()=>{
+    let nombrePersona  = document.getElementById('nombre'). value;
+    // if(nombrePersona== '' || nombrePersona == null){
+    if(!nombrePersona){
+        alert('Debe ingresar un nombre');
+    }else{
+        alert('Hola ' + nombrePersona+ '!!!!!')
+    }
+    
+});
+//add.evenlister agregar un evento al inpu
