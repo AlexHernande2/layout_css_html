5fd3702253af0611b9f8fcbca0681ce6eba3f260
Sala D16 <CSalaD16@uniboyaca.edu.co>
2023-03-16 19:26:34 -0500

    Agregacion de como s epuede llamar metodos desde html

--- js/saludar.js
+++ js/saludar.js
@@ -1,13 +1,26 @@
 // document.getElementById('nombre').setAttribute('value', 'Jhoan');
 
-document.getElementById('saludarBtn').addEventListener('click' , ()=>{
-    let nombrePersona  = document.getElementById('nombre'). value;
+
+document.getElementById('saludarBtn')
+    .addEventListener('click', () => {
+        let nombrePersona = document.getElementById('nombre').value;
+        // if(nombrePersona== '' || nombrePersona == null){
+        if (!nombrePersona) {
+            alert('Debe ingresar un nombre');
+        } else {
+            alert('Hola ' + nombrePersona + '!!!!!')
+        }
+
+    });
+
+function saludar() {
+    let nombrePersona = document.getElementById('nombre').value;
     // if(nombrePersona== '' || nombrePersona == null){
-    if(!nombrePersona){
+    if (!nombrePersona) {
         alert('Debe ingresar un nombre');
-    }else{
-        alert('Hola ' + nombrePersona+ '!!!!!')
+    } else {
+        alert('Hola ' + nombrePersona + '!!')
     }
-    
-});
+}
+
 //add.evenlister agregar un evento al inpu
